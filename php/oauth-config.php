d2d20dafef9044a96b1254c05d41cfe31a7caba5
vijay <vijayakumar.mk@zohocorp.com>
2018-03-14 18:57:01 +0530

    refactor

--- php/oauth-config.php
+++ php/oauth-config.php
@@ -395,7 +395,7 @@
 	1. Place oauth-config.php file in your server.
 
 	2. Go to URL of this file as with view-mode=home parameter
-		-> For Example : https://localhost/OAuth/php/oauth-config.php
+		-> For Example : https://localhost/OAuth/php/oauth-config.php?view-mode=home
 		
 
 	****** */
